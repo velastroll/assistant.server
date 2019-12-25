438a0a182bdfa2a7670cf1a20555ef0353affe71
alvvela <alvaro.velasco.gil@alumnos.uva.es>
2019-12-25 18:06:00 +0100

    add : user controller

--- src/controller/domain/UserCtrl.kt
+++ src/controller/domain/UserCtrl.kt
@@ -1,25 +1,30 @@
-package com.percomp.assistant.core.controller.services
+package com.percomp.assistant.core.controller.domain
 
 import com.percomp.assistant.core.config.Token
 import com.percomp.assistant.core.config.newTokens
-import com.percomp.assistant.core.dao.PeopleDAO
-import com.percomp.assistant.core.dao.UserDAO
 import com.percomp.assistant.core.model.Person
 import com.percomp.assistant.core.model.User
 import com.percomp.assistant.core.services.CredentialRequest
+import controller.services.DeviceService
+import controller.services.PeopleService
+import controller.services.UserService
 import io.ktor.auth.OAuth2Exception
 import java.lang.IllegalArgumentException
 
-class UserCtrl {
+class UserCtrl (
+    private val userService: UserService,
+    private val peopleService: PeopleService,
+    private val deviceService: DeviceService
+){
 
-    suspend fun check(auth : CredentialRequest) : Token {
+    fun check(auth : CredentialRequest) : Token {
 
         // check if user exist on db
         if (auth.user.isNullOrEmpty()) throw OAuth2Exception.InvalidGrant("not valid user.")
         if (auth.password.isNullOrEmpty()) throw OAuth2Exception.InvalidGrant("not valid password.")
 
         // check it on db
-        val user = UserDAO().check(auth.user, auth.password) ?: throw OAuth2Exception.InvalidGrant("Wrong credentials.")
+        val user = userService.check(auth.user, auth.password) ?: throw OAuth2Exception.InvalidGrant("Wrong credentials.")
 
         // store tokens
         val tokens = newTokens(username = user.username)
@@ -28,34 +33,34 @@ class UserCtrl {
         return tokens
     }
 
-    suspend fun exist(username: String) : User? {
+    fun exist(username: String) : User? {
         if (username.isNullOrEmpty()) return null
-        return UserDAO().checkExists(username)
+        return userService.getUser(username)
     }
 
-    suspend fun addPerson(person: Person) {
+    fun addPerson(person: Person) {
         if (person.nif.length < 9) throw IllegalArgumentException("Not valid nif.")
         if (person.name.length < 9) throw IllegalArgumentException("Not valid name.")
 
-        PeopleDAO().post(nif = person.nif, name = person.name, postcode = person.postcode)
+        peopleService.newPerson(nif = person.nif, name = person.name, postalCode = person.postcode)
     }
 
-    suspend fun retrievePeople(): List<Person> {
+    fun retrievePeople(): List<Person> {
         // return the list or an empty list
-        return PeopleDAO().getAll() ?: ArrayList()
+        return peopleService.getAllPeople()
     }
 
     /**
      * Retrieve all the people of a specific postal code with their device.
      */
-    suspend fun retrieve(postalcode: Int): ArrayList<Person> {
+    fun retrieve(postalcode: Int): ArrayList<Person> {
 
         // retrieve people
-        val people = UserDAO().retrieve(postalcode)
+        val people = peopleService.getByPostalCode(postalcode) as ArrayList
 
         // for each person, add their device if exists
         for (p in people){
-            p.relation = RelationDAO().getCurrentByUser(user = p.nif)
+            p.relation = deviceService.getCurrentAssignmentWithUser(p.nif)
         }
 
         return people

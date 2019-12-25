c079b002d66be774b2e5ebaf897dfd7aa9f3e80b
alvvela <alvaro.velasco.gil@alumnos.uva.es>
2019-12-25 16:21:14 +0100

    add : device controller

--- src/controller/domain/DeviceCtrl.kt
+++ src/controller/domain/DeviceCtrl.kt
@@ -1,61 +1,70 @@
-package com.percomp.assistant.core.controller.services
+package com.percomp.assistant.core.controller.domain
 
 import com.percomp.assistant.core.config.Token
 import com.percomp.assistant.core.config.newTokens
+import com.percomp.assistant.core.controller.services.LocationService
 import com.percomp.assistant.core.dao.*
 import com.percomp.assistant.core.model.*
 import com.percomp.assistant.core.services.CredentialRequest
 import com.percomp.assistant.core.util.communication.RaspiAction
+import controller.services.DeviceService
+import controller.services.PeopleService
+import controller.services.TaskService
 import io.ktor.auth.OAuth2Exception
 
-class DeviceCtrl {
+class DeviceCtrl(
+    private val deviceService : DeviceService,
+    private val taskService: TaskService,
+    private val peopleService: PeopleService,
+    private val locationService: LocationService
+    ) {
 
-    suspend fun check(auth : CredentialRequest) : Token {
+    fun check(auth : CredentialRequest) : Token {
 
         // check if user exist on db
         if (auth.user.isNullOrEmpty()) throw OAuth2Exception.InvalidGrant("Not valid user")
         if (auth.password.isNullOrEmpty()) throw OAuth2Exception.InvalidGrant("Not valid password")
         // check it on db
 
-        if (!DeviceDAO().check(auth.user, auth.password)) throw OAuth2Exception.InvalidGrant("Not valid user or password.")
+        if (!deviceService.check(auth.user, auth.password)) throw OAuth2Exception.InvalidGrant("Not valid user or password.")
         // store tokens
         val tokens = newTokens(username = auth.user)
         // return it
         return tokens
     }
 
-    suspend fun exist(mac: String) : Device? {
+    fun exist(mac: String) : Device? {
         if (mac.isNullOrEmpty()) return null
-        val device = DeviceDAO().checkExists(mac) ?: return null
+        val device = deviceService.checkExists(mac) ?: return null
         return device
     }
 
-    suspend fun create(mac : String){
+    fun create(mac : String){
         // check if user exist on db
         if (mac.isNullOrEmpty()) throw IllegalArgumentException("Invalid user")
         if (mac.length != 17 ) throw IllegalArgumentException("Invalid user")
         if (!mac.contains(":")) throw IllegalArgumentException("Invalid user")
 
         // create imei
-        DeviceDAO().post(mac = mac)
+        deviceService.post(mac = mac)
     }
 
-    suspend fun getAll(): List<Device4W> {
+    fun getAll(): List<Device4W> {
         // retrieve all
-        val devices = DeviceDAO().getAll()
+        val devices = deviceService.getAll()
         val devices4w = ArrayList<Device4W>()
         for ( d in devices ) {
             val d4 = Device4W(device = d.mac)
             // retrieve the last status
-            d4.last_status = StatusDAO().getLastFive(mac=d.mac)
+            d4.last_status = deviceService.getLastFiveStatus(mac=d.mac)
             // retrieve last events
-            d4.last_events = StatusDAO().getLastFiveTasks(mac = d.mac)
+            d4.last_events = taskService.getLastFiveTasks(mac = d.mac)
             // retrieve last intents
 
             // retrieve relation
-            d4.relation = RelationDAO().get(mac = d.mac)
+            d4.relation = deviceService.getLastAssignment(mac = d.mac)
             // retrieve pending actions
-            d4.pending = TaskDAO().getPending(d.mac)
+            d4.pending = taskService.getPendingTaskForDevice(d.mac)
             // retrieve position
 
             //add to list
@@ -65,30 +74,31 @@ class DeviceCtrl {
         return devices4w
     }
 
-    suspend fun addRelation(nif: String, device: String) {
+    fun addRelation(nif: String, device: String) {
         // check if person exists
-        val person = PeopleDAO().get(nif) ?: throw IllegalArgumentException("no person with nif = $nif.")
+        val person = peopleService.getPerson(nif) ?: throw IllegalArgumentException("no person with nif = $nif.")
 
         // check if device exists
-        val device = DeviceDAO().checkExists(mac = device) ?: throw IllegalArgumentException("no device with mac = $device.")
+        val device = deviceService.checkExists(mac = device) ?: throw IllegalArgumentException("no device with mac = $device.")
 
         // add relation
-        RelationDAO().post(nif = person.nif, device = device.mac)
+        deviceService.assignUser(nif = person.nif, mac = device.mac)
     }
 
-    suspend fun finishRelation(mac: String) {
+    fun finishRelation(mac: String) {
         // check if exist
-        val device = DeviceDAO().checkExists(mac = mac) ?: throw IllegalArgumentException("no device with mac = $mac.")
+        val device = deviceService.checkExists(mac = mac) ?: throw IllegalArgumentException("no device with mac = $mac.")
 
         // delete it
-        RelationDAO().finish(mac = device.mac)
+        deviceService.endAssignment(mac = device.mac)
 
     }
 
-    suspend fun getRelation(mac: String) : Relation?{
-        val r = RelationDAO().get(mac)
+    fun getRelation(mac: String) : Relation?{
+        val r = deviceService.getLastAssignment(mac)
         if (r!=null) {
-            val l = LocationDAO().getByPostalCode(r.user!!.postcode) ?: throw IllegalArgumentException("Does not exist a location with postal code ${r.user!!.postcode}")
+            val l = locationService.getLocationByPostalCode(postalCode = r.user!!.postcode)
+                ?: throw IllegalArgumentException("Does not exist a location with postal code ${r.user!!.postcode}")
             r.info = l.name
         }
         return r
